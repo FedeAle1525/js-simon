134eac23a7110f8e687b01514f06b09ab25322d1
FedeAle1525 <fedeale.1525@gmail.com>
2023-02-13 18:33:13 +0100

    JS code per Timing Function di controllo

--- js/script.js
+++ js/script.js
@@ -2,25 +2,32 @@
 const numbers = get5RandomNumbers();
 
 // 2. Visualizzare i 5 numeri generati tramite all'alert
-let stringNumbers = '';
-
-for (let i = 0; i < numbers.length; i++){
-  stringNumbers += ` ${numbers[i]} `;
-}
+let stringNumbers = tranfromNumbersIntoString(numbers);
 
 alert(`I NUMERI ESTRATTI sono: ${stringNumbers}`);
 
-// 3. Attendere 30 secondi prima dell'esecuzione della Funzione "getNumbersUser()"
-
-
-
+// 3. Attendere 30 secondi prima dell'esecuzione della Funzione "getNumbersUserForCheck()"
+setTimeout(getNumbersUserForCheck, 30000);
 
 
 
 
 
 // *****************************
-// function "get5RandomNumbers"
+// function "tranfromNumbersIntoString" - Crea una Stringa partendo da un Array di numeri
+// ******************************
+
+function tranfromNumbersIntoString(arrayNumbers) {
+  let stringNumbers = '';
+
+  for (let i = 0; i < arrayNumbers.length; i++) {
+    stringNumbers += ` ${arrayNumbers[i]} `;
+  }
+  return stringNumbers;
+}
+
+// *****************************
+// function "get5RandomNumbers" - Crea un Array di 5 numeri casuali da 1 a 100
 // ******************************
 
 function get5RandomNumbers (){
@@ -38,14 +45,57 @@ function get5RandomNumbers (){
   return randomsNumbers;
 }
 
+// *********************************
+// function "getNumbersUserFromPrompt" - Crea un Array di 5 numeri (compresi da 1 a 100) a chiest a Utente
+// *********************************
+
+function getNumbersUserFromPrompt(quantity){
+  let = numbersPrompt = [];
+  let count = 0;
+
+  while (count !== quantity){
+    const number = parseInt(prompt("inserisci il un numero ;)"));
+    if (isNaN(number) === false && (number >= 1 && number <=100)){
+      numbersPrompt.push(number);
+      count ++;
+    }
+  }
+
+  return numbersPrompt;
+}
+
 // *****************************
-// function "get5RandomNumbers"
+// function "getNumbersUserForCheck" - Funzione che confronta Numeri Casuali con quell inseriti da Utente
 // ******************************
 
-function getNumbersUser(){
-  const number1 = parseInt(prompt("inserisci il primo numero ;)"));
-  const number2 = parseInt(prompt("inserisci il secondo numero ;)"));
-  const number3 = parseInt(prompt("inserisci il terzo numero ;)"));
-  const number4 = parseInt(prompt("inserisci il quarto numero ;)"));
-  const number5 = parseInt(prompt("inserisci il quinto numero ;)"));
+function getNumbersUserForCheck(){
+  // Creo Array per tenre traccia dei numeri indovinati e un contatore per capire quanti numeri sono stato indovinati
+  let numbersFound = [];
+  let count = 0;
+
+  // Invoco Funzione per chiedere tot numeri a Utente
+  const numbersUser = getNumbersUserFromPrompt(5);
+
+  console.log(numbers);
+  console.log(numbersUser);
+
+  // Creo doppio Ciclo per confrontare tutti gli elementi di "arrayNumbers" con tutti gli elementi di "numberUser"
+  for (let i = 0; i < numbers.length; i++){
+    for (let j = 0; j < numbersUser.length; j++){
+      if (numbers[i] === numbersUser[j]){
+        numbersFound.push(numbers[i]);
+        count++;
+      }
+    }
+  }
+
+  const stringNumbersFound = tranfromNumbersIntoString(numbersFound);
+
+  if (count === 0){
+    console.log('Non hai indovinato nessun numero ;(');
+  } else {
+    console.log(`Complimneti! Hai indovinato ${count} numeri:${stringNumbersFound};)`);
+  }
+
+
 }
