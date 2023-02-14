d0376e1f61a252487930c7df1695bffee420c6b1
FedeAle1525 <fedeale.1525@gmail.com>
2023-02-14 10:13:15 +0100

    EDIT + REFACTOR + comments after lessons

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 const numbers = get5RandomNumbers();
 
 // 2. Visualizzare i 5 numeri generati tramite all'alert
+// Si poteva utilizzare il "metodo .join(', ')" degli Array per trasformarli in una Stringa
 let stringNumbers = tranfromNumbersIntoString(numbers);
 
 alert(`I NUMERI ESTRATTI sono: ${stringNumbers}`);
@@ -46,7 +47,7 @@ function get5RandomNumbers (){
 }
 
 // *********************************
-// function "getNumbersUserFromPrompt" - Crea un Array di 5 numeri (compresi da 1 a 100) a chiest a Utente
+// function "getNumbersUserFromPrompt" - Crea un Array di 5 numeri (compresi da 1 a 100) a chiesto a Utente
 // *********************************
 
 function getNumbersUserFromPrompt(quantity){
@@ -55,10 +56,10 @@ function getNumbersUserFromPrompt(quantity){
 
   while (count !== quantity){
     const number = parseInt(prompt("inserisci il un numero ;)"));
-    if (isNaN(number) === false && (number >= 1 && number <=100)){
+    // if (isNaN(number) === false && (number >= 1 && number <=100)){
       numbersPrompt.push(number);
       count ++;
-    }
+    // }
   }
 
   return numbersPrompt;
@@ -69,8 +70,9 @@ function getNumbersUserFromPrompt(quantity){
 // ******************************
 
 function getNumbersUserForCheck(){
-  // Creo Array per tenre traccia dei numeri indovinati e un contatore per capire quanti numeri sono stato indovinati
+  // Creo Array per tenere traccia dei numeri indovinati e un contatore per capire quanti numeri sono stato indovinati
   let numbersFound = [];
+  // Avendo un Array di Numeri Indovinati, il contatore è superfluo --> al suo posto si può usare ".lenght" 
   let count = 0;
 
   // Invoco Funzione per chiedere tot numeri a Utente
@@ -80,9 +82,11 @@ function getNumbersUserForCheck(){
   console.log(numbersUser);
 
   // Creo doppio Ciclo per confrontare tutti gli elementi di "arrayNumbers" con tutti gli elementi di "numberUser"
+  // Un'alternativa era quella di fare il controllo all'inserimento dei numeri da Prompt usando ".includes"
   for (let i = 0; i < numbers.length; i++){
     for (let j = 0; j < numbersUser.length; j++){
-      if (numbers[i] === numbersUser[j]){
+      // Devo controllare che l'Utente non faccia il furbo inserimento lo stesso numero più volte  
+      if (numbers[i] === numbersUser[j] && numbersFound.includes(numbers[i]) === false){
         numbersFound.push(numbers[i]);
         count++;
       }
@@ -94,7 +98,7 @@ function getNumbersUserForCheck(){
   if (count === 0){
     console.log('Non hai indovinato nessun numero ;(');
   } else {
-    console.log(`Complimneti! Hai indovinato ${count} numeri:${stringNumbersFound};)`);
+    console.log(`Complimenti! Hai indovinato ${count} numeri:${stringNumbersFound};)`);
   }
 
 
